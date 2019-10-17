7ac59208d88c83375b4b3c66dbede3522bce3430
Insurikai <40343703+Insurikai@users.noreply.github.com>
2019-10-17 12:58:15 -0700

    Implement Form Validation and Error Messaging

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
-const MyForm = () =>{
+const MyForm = ({touched, errors}) =>{
     return (
-        <Form>
-            <Field type="text" name="username" id="username" placeholder="Username"/>
-            <Field type="email" name="email" id="email" placeholder="Email"/>
-            <Field type="password" name="password" id="password" placeholder="Password"/>
-            <Field type="checkbox" name="tos" id="tos"/>
+        <Form className="myForm">
+            <div className="holder">
+                <Field type="text" name="username" id="username" placeholder="Username"/>
+                {touched.username && errors.username && ( <p className="error">{errors.username}</p> )}
+            </div>
+            <div className="holder">
+                <Field type="email" name="email" id="email" placeholder="Email"/>
+                {touched.email && errors.email && ( <p className="error">{errors.email}</p> )}
+            </div>
+            <div className="holder">
+                <Field type="password" name="password" id="password" placeholder="Password"/>
+                {touched.password && errors.password && ( <p className="error">{errors.password}</p> )}
+            </div>
+            <div className="holder">
+                <Field type="checkbox" name="tos" id="tos"/>
+                {touched.tos && errors.tos && ( <p className="error">{errors.tos}</p> )}
+            </div>
             <button type="submit">Register</button>
         </Form>
     );
@@ -19,7 +32,13 @@ const FormikForm = withFormik({
             email: email || "",
             password: password || "",
             tos: tos || false
-        }
-    }
+        };
+    },
+    validationSchema: Yup.object().shape({
+        username: Yup.string().trim().min(4, 'Username is too short.').required("Required"),
+        email: Yup.string().trim().email().required("Required"),
+        password: Yup.string().min(8, 'Password is too short.').required("Required"),
+        tos: Yup.bool().oneOf([true], 'You must accept the ToS')
+    })
 })(MyForm);
 export default FormikForm;
